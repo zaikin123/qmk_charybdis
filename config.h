4f304196a4731bb2c2eae2de1a2f2ae45807e2a9
mralex07 <50528263+mralex07@users.noreply.github.com>
2022-09-30 21:07:53 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 #pragma once
 
 /* Key matrix configuration. */
+#define MATRIX_ROWS 10 // Rows are doubled-up.
+#define MATRIX_COLS 6
 #define MATRIX_ROW_PINS \
     { F1, B7, C6, D4, B5 }
 #define MATRIX_COL_PINS \
@@ -10,6 +12,16 @@
     { D5, F0, C6, D4, B5 }
 #define MATRIX_COL_PINS_RIGHT \
     { F1, C7, B7, D7, E6, B4 }
+
+#define DIODE_DIRECTION ROW2COL
+
+/* Set 0 if debouncing isn't needed. */
+#define DEBOUNCE 5
+
+/* Trackball angle adjustment. */
+#define ROTATIONAL_TRANSFORM_ANGLE -25
+
+
 
 /* Handedness. */
 #define MASTER_RIGHT
