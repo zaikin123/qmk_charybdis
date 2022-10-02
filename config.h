6681b83a6204dfee74a0d3a6a886847e67446fe7
mralex07 <50528263+mralex07@users.noreply.github.com>
2022-10-02 18:40:56 +0300

    Update config.h

--- config.h
+++ config.h
@@ -1,15 +1,16 @@
 #pragma once
 
 /* Key matrix configuration. */
-#define MATRIX_ROWS 10 // Rows are doubled-up.
-#define MATRIX_COLS 6
+
+#undef MATRIX_ROW_PINS
 #define MATRIX_ROW_PINS \
     { F4, F5, F6, F7, B6 }
+#undef MATRIX_COL_PINS
 #define MATRIX_COL_PINS \
     { D4, C6, D7, E6, B4 , B5 }
 
 
-#define DIODE_DIRECTION ROW2COL
+
 
 /* Set 0 if debouncing isn't needed. */
 #define DEBOUNCE 5
@@ -23,10 +24,12 @@
 #define MASTER_RIGHT
 
 /* serial.c configuration (for split keyboard). */
+#undef SOFT_SERIAL_PIN
 #define SOFT_SERIAL_PIN D0
 
 /* RGB settings. */
 /* #define RGB_DI_PIN D3 */
 
 /* PMW3360 settings. */
+#undef PMW33XX_CS_PIN
 #define PMW33XX_CS_PIN D1
