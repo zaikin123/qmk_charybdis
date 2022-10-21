25110127593a7b54951a0d05d837b923db290ccc
mralex07 <50528263+mralex07@users.noreply.github.com>
2022-10-22 00:36:05 +0300

    Update config.h

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 
 #undef MATRIX_ROW_PINS
 #define MATRIX_ROW_PINS \
-    { F4, F5, F6, F7, B6 }
+    { F4, F5, F6, F7, B5 }
 #undef MATRIX_COL_PINS
 #define MATRIX_COL_PINS \
-    { D4, C6, D7, E6, B4 , B5 }
+    { D1, D4, C6, D7, E6, B4 }
 
 #undef DIODE_DIRECTION
 #define DIODE_DIRECTION COL2ROW
@@ -32,4 +32,4 @@
 
 /* PMW3360 settings. */
 #undef PMW33XX_CS_PIN
-#define PMW33XX_CS_PIN D2
+#define PMW33XX_CS_PIN B6
