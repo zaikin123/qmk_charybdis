f6a9cf186bd7ea9a7e812d58a1fc050a23b00627
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-11 19:39:15 +0200

    Update mralex07.h

--- mralex07.h
+++ mralex07.h
@@ -1,23 +1,4 @@
-#define LAYER_BASE 0
-#define LAYER_POINTER 1
-
-#ifndef POINTING_DEVICE_ENABLE
-#    define DRGSCRL KC_NO
-#    define DPI_MOD KC_NO
-#    define S_D_MOD KC_NO
-#    define SNIPING KC_NO
-#endif // !POINTING_DEVICE_ENABLE
-
-#ifdef VIA_ENABLE
-/* VIA configuration. */
-#    define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#endif // VIA_ENABLE
-
-/* Disable unused features. */
-#define NO_ACTION_ONESHOT
-
-/* Charybdis-specific features. */
-
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS 1000
-#define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
+enum charybdis_keymap_layers {
+    LAYER_BASE,
+    LAYER_POINTER,
+};
