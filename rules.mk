9f6927fb6540178af0a15df2719d6b4fac8091db
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-14 18:57:50 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -26,8 +26,8 @@ SPLIT_KEYBOARD = yes
 
 LAYOUTS = split_3x5_3
 
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+#POINTING_DEVICE_ENABLE = yes
+#POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
 KEYBOARD_SHARED_EP = yes
 
