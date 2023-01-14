5c78f788af9ee6ca2403b32a0111ed92c43042df
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-14 18:44:00 +0200

    Update mcuconf.h

--- mcuconf.h
+++ mcuconf.h
@@ -19,8 +19,8 @@
 
 #include_next <mcuconf.h>
 
-#undef STM32_SPI_USE_SPI1
-#define STM32_SPI_USE_SPI1 TRUE
+#undef STM32_I2S_USE_SPI2
+#define STM32_I2S_USE_SPI2 TRUE
 
 #undef STM32_SERIAL_USE_USART1
 #define STM32_SERIAL_USE_USART1 FALSE
