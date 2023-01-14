f9149a2a105fbc3bbb4489b07ef426de8c6af8e3
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-14 18:31:51 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -19,15 +19,9 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = no
-RGB_MATRIX_DRIVER = WS2812
-LTO_ENABLE = yes
 
-# if firmware size over limit, try this option
-# LTO_ENABLE = yes
+SPLIT_KEYBOARD = yes
 
 LAYOUTS = split_3x5_3
 
