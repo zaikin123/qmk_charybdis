021603c256f1beef9f5fe3f6f335f5c5d10e0cb6
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-14 15:22:00 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,4 +1,33 @@
 SRC += source.c
-BOOTMAGIC_ENABLE = yes
+# MCU name
+MCU = STM32F411
+
+# Bootloader selection
+BOOTLOADER = stm32-dfu
+CONVERT_TO = stemcell
+#STMC_US = yes
+
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = no
+RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
-VIA_ENABLE = yes
+
+# if firmware size over limit, try this option
+# LTO_ENABLE = yes
+
+LAYOUTS = split_3x5_3
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
