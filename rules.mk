495bb0bef5fb108bd505dd19ac8f50be24a88b41
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-14 18:33:06 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -27,6 +27,8 @@ LAYOUTS = split_3x5_3
 
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
+KEYBOARD_SHARED_EP = yes
 
 SERIAL_DRIVER = usart
 DEBOUNCE_TYPE = asym_eager_defer_pk
