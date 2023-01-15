d97339282611a3954cedb7875d08f91ca6c034a4
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-15 19:00:50 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,4 +1,5 @@
 SRC += source.c
-BOOTMAGIC_ENABLE = yes
-LTO_ENABLE = yes
-VIA_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
+KEYBOARD_SHARED_EP = yes
