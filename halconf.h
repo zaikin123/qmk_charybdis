f100fb329e76483a1d15c1b174c5147269aa0158
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-17 23:58:09 +0200

    Update halconf.h

--- halconf.h
+++ halconf.h
@@ -17,8 +17,8 @@
 
 #pragma once
 
-#define HAL_USE_PWM TRUE
-#define HAL_USE_SERIAL TRUE
+/* #define HAL_USE_PWM TRUE
+#define HAL_USE_SERIAL TRUE */
 #define HAL_USE_I2C TRUE
 
 #include_next <halconf.h>
