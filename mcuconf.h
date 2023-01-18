ea5c26213331d5fb4dabd0fea917a428755b8704
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-18 22:36:36 +0200

    Update mcuconf.h

--- mcuconf.h
+++ mcuconf.h
@@ -18,13 +18,13 @@
 #pragma once
 
 #include_next <mcuconf.h>
-/*
+
 #undef STM32_SERIAL_USE_USART1
 #define STM32_SERIAL_USE_USART1 FALSE
 
 #undef STM32_PWM_USE_TIM2
 #define STM32_PWM_USE_TIM2 TRUE
-*/
+
 #undef STM32_ST_USE_TIMER
 #define STM32_ST_USE_TIMER 5
 
