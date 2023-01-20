6a35e2ce6e470bf93124d3c0ec4ed8fa63020d76
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-01-20 18:37:19 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -2,7 +2,7 @@ SRC += source.c
 MCU_FAMILY = NRF52
 MCU  = cortex-m4
 ARMV = 7
-BOOTLOADER = tinyuf2
+
 # BOOTMAGIC_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
