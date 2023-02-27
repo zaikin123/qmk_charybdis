f993c777c4c6f07738bfd7757ef28c3e45d4a742
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-02-27 14:09:31 +0200

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,12 @@
 #undef  DIODE_DIRECTION
 #define DIODE_DIRECTION ROW2COL
 #undef MATRIX_COL_PINS
-#define MATRIX_COL_PINS { D2, D1, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS \
+    { F6, F5, B6, D7, E6, B4 }
 #undef MATRIX_ROW_PINS
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS \
+    { F4, F7, C6, D4, B5 }
+
+
+/* Handedness. */
+#define MASTER_LEFT
