b1cc7c944bb62b5eb1e2b32cd9a3dcbd50f69bec
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-04 23:39:33 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,14 +1,14 @@
 SRC += source.c
 # MCU name
-# MCU = RP2040
+MCU = RP2040
 
 # Bootloader selection
-# BOOTLOADER = rp2040
+BOOTLOADER = rp2040
 
 # RP2040-specific options
 ALLOW_WARNINGS = yes
 PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
 SERIAL_DRIVER = vendor
-LAYOUT = LAYOUT_split_3x5_2
+
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
