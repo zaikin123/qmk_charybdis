4d5a3cd878175b303d14753d68a844898dd65325
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 21:54:34 +0200

    Update mcuconf.h

--- mcuconf.h
+++ mcuconf.h
@@ -19,5 +19,12 @@
 
 #include_next "mcuconf.h"
 
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
+#pragma once
+
+#include_next <mcuconf.h>
+
+#undef RP_I2C_USE_I2C1
+#define RP_I2C_USE_I2C1 FALSE
+
+#undef RP_I2C_USE_I2C0
+#define RP_I2C_USE_I2C0 TRUE
