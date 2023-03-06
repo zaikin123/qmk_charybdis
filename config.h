343c44539a7e1e2e954bb63c455fd42ee310c08f
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-06 15:51:34 +0200

    Update config.h

--- config.h
+++ config.h
@@ -2,8 +2,9 @@
 #define PRODUCT "a_dux"
 #undef MATRIX_ROW_PINS
 #undef MATRIX_COL_PINS
-#define DIRECT_PINS { { GP29, GP23, GP26, GP1,  GP5}, { GP28, GP20, GP27, GP3, GP6 } , { GP21, GP22, GP0, GP4, GP7 } , { GP9, GP8 }}
-#define DIRECT_PINS_RIGHT { { GP5, GP1, GP26, GP23,  GP29}, { GP6, GP3, GP27, GP20, GP28 } , { GP7, GP4, GP0, GP22, GP21 } , { GP8, GP9 }}
+#define DIRECT_PINS { { GP5, GP1, GP26, GP23,  GP29}, { GP6, GP3, GP27, GP20, GP28 } , { GP7, GP4, GP0, GP22, GP21 } , { GP8, GP9 }}
+// #define DIRECT_PINS { { GP29, GP23, GP26, GP1,  GP5}, { GP28, GP20, GP27, GP3, GP6 } , { GP21, GP22, GP0, GP4, GP7 } , { GP9, GP8 }}
+// #define DIRECT_PINS_RIGHT { { GP5, GP1, GP26, GP23,  GP29}, { GP6, GP3, GP27, GP20, GP28 } , { GP7, GP4, GP0, GP22, GP21 } , { GP8, GP9 }}
 
 #undef SOFT_SERIAL_PIN
 #define SOFT_SERIAL_PIN GP2
@@ -12,10 +13,10 @@
 #define I2C_DRIVER I2CD1
 #undef I2C1_SDA_PIN
 #define I2C1_SDA_PIN GP14
-#define I2C1_SDA_PAL_MODE 4
+// #define I2C1_SDA_PAL_MODE 4
 #undef I2C1_SCL_PIN
 #define I2C1_SCL_PIN GP15
-#define I2C1_SCL_PAL_MODE 4
+// #define I2C1_SCL_PAL_MODE 4
 
 /* Handedness. */
 #undef EE_HANDS
