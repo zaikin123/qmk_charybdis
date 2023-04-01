ad07f52dae8eba1857e0bb7b96774dfc51346d01
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-01 23:07:32 +0300

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -2,6 +2,7 @@ SRC += source.c
 # RP2040-specific options
 ALLOW_WARNINGS = yes
 PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+BOOTLOADER = tinyuf2
 
 # Build Options
 #   change yes to no to disable
