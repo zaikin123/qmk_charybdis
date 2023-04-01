7a03fae133e05a4e806360978c190b1bee76426b
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-01 22:36:18 +0300

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,4 +1,26 @@
 SRC += source.c
-BOOTMAGIC_ENABLE = yes
-LTO_ENABLE = yes
-VIA_ENABLE = yes
+# RP2040-specific options
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+AUDIO_SUPPORTED = no        # Audio is not supported
+RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
+RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
+# RGB_MATRIX_DRIVER = WS2812
+
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
