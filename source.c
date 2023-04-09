0bcae6714c8bacb84d496857c6cfc91090e319bf
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-09 17:45:24 +0300

    Update source.c

--- source.c
+++ source.c
@@ -3,13 +3,13 @@
 
 
 
-// Tap Dance definitions
+/*// Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_RBRC_ENT] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_ENT),
     [TD_LBRC_SPC] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_SPC),
     [TD_QUOT_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_TAB),
-};
+}; */
 
 
 
