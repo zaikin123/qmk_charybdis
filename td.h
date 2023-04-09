388562f6f563a17a01e7e4ac14ae4c43311da05c
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-09 19:33:57 +0300

    Update td.h

--- td.h
+++ td.h
@@ -17,3 +17,17 @@ typedef enum {
     TD_SINGLE_HOLD,
     TD_DOUBLE_SINGLE_TAP
 } td_state_t;
+
+// Create a global instance of the tapdance state type
+static td_state_t td_state;
+
+// Declare your tapdance functions:
+
+// Function to determine the current tapdance state
+td_state_t cur_dance(tap_dance_state_t *state);
+
+// `finished` and `reset` functions for each tapdance keycode
+void sftlbrcspc_finished(tap_dance_state_t *state, void *user_data);
+void sftlbrcspc_reset(tap_dance_state_t *state, void *user_data);
+void lt1rbrcent_finished(tap_dance_state_t *state, void *user_data);
+void lt1rbrcent_reset(tap_dance_state_t *state, void *user_data);
