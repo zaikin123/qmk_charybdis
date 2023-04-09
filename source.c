33401dc9b52f2becb0ad6e3957e70f278cc6474c
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-09 14:36:34 +0300

    Update source.c

--- source.c
+++ source.c
@@ -15,14 +15,14 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_QUOT_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, KC_TAB),
 };
 
-// Add tap dance item to your keymap in place of a keycode
+/*// Add tap dance item to your keymap in place of a keycode
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ...
     TD(TD_RBRC_ENT),
     TD(TD_LBRC_SPC),
     TD(TD_QUOT_TAB)
     // ...
-};
+};*/
 
 const uint16_t PROGMEM bspc_combo[] = {KC_LBRC, KC_RBRC, COMBO_END};
 const uint16_t PROGMEM del_combo[] = {KC_QUOT, KC_LCTL, COMBO_END};
