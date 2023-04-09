5013ffd4924bf47c8d72831ead4d0e65ef686281
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-09 17:40:46 +0300

    Create TD.h

---
+++ TD.h
@@ -0,0 +1,16 @@
+// Tap Dance keycodes
+enum td_keycodes {
+    LSFT_LBRC_SPC,
+    LT1_RBRC_ENT
+    TD_RBRC_ENT,
+    TD_LBRC_SPC,
+    TD_QUOT_TAB
+};
+// Define a type containing as many tapdance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP
+} td_state_t;
