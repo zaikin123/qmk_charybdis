39b660715ba457ab48b37e3722c105a01259198c
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-04-09 20:27:05 +0300

    Update td.h

--- td.h
+++ td.h
@@ -31,3 +31,99 @@ void sftlbrcspc_finished(tap_dance_state_t *state, void *user_data);
 void sftlbrcspc_reset(tap_dance_state_t *state, void *user_data);
 void lt1rbrcent_finished(tap_dance_state_t *state, void *user_data);
 void lt1rbrcent_reset(tap_dance_state_t *state, void *user_data);
+
+// Determine the tapdance state to return
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    }
+
+    if (state->count == 2) return TD_DOUBLE_SINGLE_TAP;
+    else return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+// Handle the possible states for each tapdance keycode you define:
+
+void sftlbrcspc_finished(tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code16(KC_LBRC);
+            break;
+        case TD_SINGLE_HOLD:
+            register_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
+            tap_code16(KC_SPC);
+            register_code16(KC_SPC);
+            break;
+        default:
+            break;
+    }
+}
+
+void sftlbrcspc_reset(tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code16(KC_LBRC);
+            break;
+        case TD_SINGLE_HOLD:
+            unregister_mods(MOD_BIT(KC_LSFT)); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP:
+            unregister_code16(KC_SPC);
+            break;
+        default:
+            break;
+    }
+}
+
+// LT1_RBRC_ENT
+void lt1rbrcent_finished(tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code16(KC_RBRC);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(1); // For a layer-tap key, use `layer_on(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP: // Allow nesting of 2 parens `((` within tapping term
+            tap_code16(KC_ENT);
+            register_code16(KC_ENT);
+            break;
+        default:
+            break;
+    }
+}
+
+void lt1rbrcent_reset(tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code16(KC_RBRC);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_off(1); // For a layer-tap key, use `layer_off(_MY_LAYER)` here
+            break;
+        case TD_DOUBLE_SINGLE_TAP:
+            unregister_code16(KC_ENT);
+            break;
+        default:
+            break;
+    }
+}
+
+// Define `ACTION_TAP_DANCE_FN_ADVANCED()` for each tapdance keycode, passing in `finished` and `reset` functions
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_LSFT_LBRC_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sftlbrcspc_finished, sftlbrcspc_reset),
+    [TD_LT1_RBRC_ENT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, lt1rbrcent_finished, lt1rbrcent_reset)
+};
+
+// Combos 
+const uint16_t PROGMEM bspc_combo[] = {KC_LBRC, KC_RBRC, COMBO_END};
+const uint16_t PROGMEM del_combo[] = {KC_QUOT, KC_LCTL, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(bspc_combo, KC_BSPC),
+    COMBO(del_combo, KC_DEL), // keycodes with modifiers are possible too!
+};
