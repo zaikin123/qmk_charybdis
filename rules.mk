1c4d6920bfa4c0f85db6083c8c7edad2845f3014
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-12-09 00:45:51 +0200

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -26,7 +26,7 @@ AUDIO_ENABLE = no           # Audio output
 AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
+RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
 RGB_MATRIX_DRIVER = WS2812
 
 SPLIT_KEYBOARD = yes
